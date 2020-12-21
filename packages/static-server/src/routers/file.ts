ba0a2eb834aaaed5532b01ad8f65fc408c05675d
luohuidong <luohuidong01@126.com>
2020-12-21 22:57:07 +0800

    refactor: 在依赖文件夹的逻辑之前检测文件夹的路径

--- packages/static-server/src/routers/file.ts
+++ packages/static-server/src/routers/file.ts
@@ -2,6 +2,7 @@ import express from "express";
 import fs from "fs";
 import path from "path";
 import AdmZip from "adm-zip";
+import { FsUtils } from "@luohuidong/publish-utilities";
 
 const router = express.Router({});
 
@@ -9,12 +10,28 @@ router.post("/:filename", (req, res) => {
   const params = req.params;
   const filename = params.filename;
 
+  const fsUtils = FsUtils.getInstance();
+
+  // 将 zip 包保存到 tmp 目录中
   const zipFilePath = path.join(__dirname, "../../tmp/", filename);
+  fsUtils.checkDirPath(zipFilePath);
   const writableStream = fs.createWriteStream(zipFilePath);
+
   writableStream.on("close", () => {
     try {
+      const publicPath = path.join(__dirname, "../../public/");
+      fsUtils.checkDirPath(publicPath);
+
+      // 清除 public 文件夹中的所有内容
+      fs.rmSync(publicPath, {
+        recursive: true,
+        force: true,
+      });
+
+      // 将上传的 zip 包解压到 public 文件夹中
       const zip = new AdmZip(zipFilePath);
-      zip.extractAllTo(path.join(__dirname, "../../public/"), true);
+      zip.extractAllTo(publicPath, true);
+
       res.end();
     } catch (error) {
       res.status(500).send({
