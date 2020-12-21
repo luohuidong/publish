2cd5dd52258a80944619669fc9db77e0348d91f3
luohuidong <luohuidong01@126.com>
2020-12-21 12:34:16 +0800

    refactor: 文件夹路径从命令行参数中获取

--- packages/publish-tool/src/index.ts
+++ packages/publish-tool/src/index.ts
@@ -1,13 +1,13 @@
 import path from "path";
 import UploadFile from "./UploadFile";
 
-// // 获取文件路径
-// let filePath = process.argv[2];
-// if (!filePath) {
-//   throw new Error("缺少路径参数");
-// }
-// if (!/^\//.test(filePath)) {
-//   filePath = path.join(process.cwd(), filePath);
-// }
+// 获取文件夹路径
+let dirPath = process.argv[2];
+if (!dirPath) {
+  throw new Error("缺少路径参数");
+}
+if (!/^\//.test(dirPath)) {
+  dirPath = path.join(process.cwd(), dirPath);
+}
 
-new UploadFile().main(path.join(__dirname, "../dist"));
+new UploadFile().main(dirPath);
